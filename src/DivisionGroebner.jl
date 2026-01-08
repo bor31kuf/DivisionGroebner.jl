753e573bf0e45228d9c369774da222f002f35cf6
Joris <bor31kuf@rptu.de>
2026-01-08 12:32:32 +0100

    hey

--- src/DivisionGroebner.jl
+++ src/DivisionGroebner.jl
@@ -4,11 +4,8 @@ using Oscar, SIMD,DataStructures
 
 include("CircularSIMDNormal.jl")
 include("SIMDArrayNorm.jl")
-<<<<<<< HEAD
+
 export DIVCirc, DIVArray, PolNeuCirc, PolNeuArray, NeuPolArray, NeuPolCirc, DIVCircC, DIVCircA
-=======
-export DIVCirc, DIVArray, PolNeuCirc, PolNeuArray, NeuPolArray, NeuPolCirc, DIVCircC
->>>>>>> 04b24575486f58347902c07a1642d41cd2c79c3b
 
 end
 
