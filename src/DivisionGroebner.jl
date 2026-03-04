aafcc801bca74e87d7be53798385de2a2c375eae
Joris <bor31kuf@rptu.de>
2026-03-04 13:00:21 +0100

    ain

--- src/DivisionGroebner.jl
+++ src/DivisionGroebner.jl
@@ -5,14 +5,14 @@ using Oscar, SIMD,DataStructures
 include("CircularSIMDNormal.jl")
 include("SIMDArrayNorm.jl")
 include("GroebnerSIMDNorm.jl")
-include("OscarGeo.jl")
+#include("OscarGeo.jl")
 include("OhneGeobucket.jl")
 include("AlteStruktur.jl")
 include("CircularNormal.jl")
 include("CircularSIMDNormalKo.jl")
 
 
-export DIVCirc, DIVArray, PolNeuCirc, PolNeuArray, NeuPolArray, NeuPolCirc, DIVCircC, DIVArrayC, GroebnerCirc, Gewicht, DIVArrayOhneGeoC, DIVArrayOhneGeo, DivRestFam, PolNeuCirc2, NeuPolCirc2, DIVCirc2, DIVCircC2,DivRestFam,DIVCircCKo, DIVOscar
+export DIVCirc, DIVArray, PolNeuCirc, PolNeuArray, NeuPolArray, NeuPolCirc, DIVCircC, DIVArrayC, GroebnerCirc, Gewicht, DIVArrayOhneGeoC, DIVArrayOhneGeo, DivRestFam, PolNeuCirc2, NeuPolCirc2, DIVCirc2, DIVCircC2,DivRestFam,DIVCircCKo
 
 end
 
